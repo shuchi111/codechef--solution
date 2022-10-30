cbbc2343b3cbe70d9cde4de124783da32d921c59
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-10-30 17:57:09 +0530

    Create reach on time.cc

---
+++ reach on time.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r",stdin);
+   freopen("output.txt", "w",stdout);
+#endif
+
+int vem; 
+cin>>vem;
+while(vem--){
+    int vaishu; cin>>vaishu;
+    (vaishu>=30)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+}
+}
