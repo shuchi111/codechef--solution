2295bc110e6d00d7cc7149e42ad4452aa5151785
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-10-31 01:37:32 +0530

    Create fit.cc

---
+++ fit.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	int a;
+	for(int i=0;i<n;i++)
+	{
+	    cin>>a;
+	    //cin>>a[1];
+	    int s=a*10;
+	    cout<<s<<endl;
+	}
+	return 0;
+}
