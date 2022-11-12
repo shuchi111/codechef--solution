a537882a46b17687ba314fd719a1681c83b4b4ab
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-11-12 07:51:40 +0530

    Create determine the score.cc

---
+++ determine the score.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++){
+	    int X,N,pqm,s;
+	    cin>>X;
+	    cin>>N;
+	    pqm=X/10;
+	    s=pqm*N;
+	    cout<<s<<endl;
+	    
+	}
+
+}
