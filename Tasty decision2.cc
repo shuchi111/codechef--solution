1d537aed95efae5ed117fd5566b0dccd6ea0b539
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-11-14 04:40:32 +0530

    Create Tasty decision2.cc

---
+++ Tasty decision2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    if (2*x>5*y){
+	        cout<<"Chocolate"<<endl;
+	    }
+	    else if (5*y>2*x){
+	        cout<<"Candy"<<endl;
+	    }
+	    else{
+	        cout<<"Either"<<endl;
+	    }
+	}
+	return 0;
+}
