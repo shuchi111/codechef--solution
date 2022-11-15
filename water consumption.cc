39eb3cfa89a681abd51be8397c746b02013b2fe8
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-11-16 04:10:43 +0530

    Create water consumption.cc

---
+++ water consumption.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(X>=2000){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+}
