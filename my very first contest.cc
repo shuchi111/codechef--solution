11b2d997fd3cf06764d785aba15f7335287d5423
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-11-21 02:39:09 +0530

    Create my very first contest.cc

---
+++ my very first contest.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,a,b;
+	
+
+	    cin >> n>> a >> b;
+	    float s1,s2;
+	     s1 = n-a;
+	     s2 = s1-b;
+	    cout << s1 <<" "<< s2<< endl;
+	return 0;
+}
