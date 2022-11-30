d3e3cec038411442110c5dd1569b1b6a27bd77ea
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-12-01 03:19:38 +0530

    Create minimum number of ones.cc

---
+++ minimum number of ones.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+/* int n; cin>>n; string s; cin>>s; for(int i=0;i<s.size();i++)*/
+using namespace std;int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+ int n;
+    cin>>n;
+    cout<<n/2<<endl;
+
+}
+}
