3a529bcf8dbea3cc64c3710ed19474729f56eed0
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-11-30 17:20:19 +0530

    Create squats.cc

---
+++ squats.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+    int X;
+    cin>>X;
+    cout<<X*15<<"\n";
+    }
+    
+	// your code goes here
+	return 0;
+}
