b36c367561fd915b6e5efc5611afdd3afba7101a
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-12-14 03:21:16 +0530

    Chess Time

---
+++ chess Time.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    cout<<60*n/20<<endl;
+	    
+	}
+	return 0;
+}
